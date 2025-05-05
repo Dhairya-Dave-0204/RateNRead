3b3f302565d37700c7d0dc73043c2ddd77f47334
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-05-05 10:56:17 +0530

    Create the basic setup for server

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import pg from "pg"
+import cors from "cors"
+import "dotenv/config"
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
