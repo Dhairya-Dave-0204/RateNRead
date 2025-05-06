fd8afa2df5fc9f261ff9b5eff85fc3f165d13e7f
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-05-06 16:39:18 +0530

    Create routes for authentication and minor bug fixes

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -57,7 +57,7 @@ passport.use(
     {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/profile",
+      callbackURL: "http://localhost:3000/auth/google/callback",
     },
     async (accessToken, rereshToken, profile, cb) => {
       try {
---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,92 @@
+import express from "express";
+import passport from "passport";
+import bcrypt from "bcrypt";
+import { createUser, findUserByEmail } from "../models/userModel.js";
+
+const router = express.Router();
+
+// ===== LOCAL AUTH ROUTES ===== //
+
+// Registration (Manual setup)
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.send({
+      success: false,
+      message: "Please fill in all fields, eror from authRoutes",
+    });
+  }
+
+  try {
+    const existingUser = await findUserByEmail(email);
+    if (existingUser)
+      res.send({ success: false, message: "User already exist via authRoute" });
+
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 is number of salt rounds
+    const newUser = await createUser(username, email, hashedPassword);
+
+    // Automatically login the user after registration
+    req.login(newUser, (error) => {
+      if (error) {
+        return res.send({
+          success: false,
+          message: "Error logging in user after registration via authRoute",
+        });
+      }
+      return res.send({ success: true, message: "Registration successful" });
+    });
+  } catch (error) {
+    console.log("Error creating a new user via authRoutes" + error);
+    res.send({
+      success: false,
+      message: "Error creating a new user via authRoutes",
+    });
+  }
+});
+
+// Login (Local strategy)
+router.post("/login", (req, res, cb) => {
+    passport.authenticate("local", (error, user, info) => {
+        if (error) return cb(error);
+        if(!user) return res.send({ success: false, message: "Invalid credentials via authRoute" });
+
+        req.login(user, (error) => {
+            if (error) return cb(error)
+            return res.send({ success: true, message: "Login successful", user });
+        })
+    }) (req, res, cb)
+});
+
+// ===== GOOGLE AUTH ROUTES ===== //
+
+// Redirect to google
+router.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+)
+
+// Google callback
+router.get("/auth/google/callback",
+    passport.authenticate("google", {
+        failureRedirect: "/login",
+        session: true
+    }), 
+    (req, res) => {
+        // On successful authentication by google, redirect to the dashboard
+        res.redirect("/profile");
+    }
+)
+
+// ===== LOGOUT ROUTE ===== //
+router.get("/logout", (req, res) => {
+  req.logout((error) => {
+    if (error) {
+      console.log("Error logging out" + error);
+      return res.send({
+        success: false,
+        message: "Error logging out user via authRoute",
+      });
+    }
+    res.redirect("/");
+  });
+});
