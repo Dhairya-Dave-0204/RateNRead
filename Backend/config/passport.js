1c3ec235dea081346b53f7b5aa0575af43467622
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-05-06 15:00:26 +0530

    Create passport config file for authentication and google session

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -15,22 +15,91 @@ import {
   findUserByGoogleId,
 } from "../models/userModel.js";
 
+import {
+  createSession,
+  deleteSessions,
+  findSessionByToken,
+} from "../models/sessionModel.js";
+
 // Local strategy setup for enabling the local sessions and user management
-passport.use("local", 
-    new LocalStrategy(async (email, password, cb) => {
-        try {
-            // Finding user by using the email and checking if the user exist or not
-            const user = await findUserByEmail(email);
-            if(!user) return cb(null, false, { message: "Invalid credentials. User not found!" });
+passport.use(
+  "local",
+  new LocalStrategy(async (email, password, cb) => {
+    try {
+      // Finding user by using the email and checking if the user exist or not
+      const user = await findUserByEmail(email);
+      if (!user)
+        return cb(null, false, {
+          message: "Invalid credentials. User not found!",
+        });
 
-            // Checking the pass enterd by the user with the password stored in the database
-            const isValid = await bcrypt.compare(password, user.pass)
-            if(!isValid) return cb(null, false, { message: "Invalid Password"})
+      // Checking the pass enterd by the user with the password stored in the database
+      const isValid = await bcrypt.compare(password, user.pass);
+      if (!isValid) return cb(null, false, { message: "Invalid Password" });
 
-            // deleting any old sessions and creating a fresth new session
-        } catch (error) {
-            console.log("Error using the local strategy for management" + error);
-            return cb(error);
-        }
-    })
+      // deleting any old sessions and creating a fresth new session
+      await deleteSessions(user.id);
+      const session = await createSession(user.id);
+
+      user.sessionToken = session.token;
+      return cb(null, user);
+    } catch (error) {
+      console.log("Error using the local strategy for management" + error);
+      return cb(error);
+    }
+  })
 );
+
+// Google strategy setup for enabling the google sessions and user management
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/profile",
+    },
+    async (accessToken, rereshToken, profile, cb) => {
+      try {
+        let user = await findUserByGoogleId(profile.id);
+        if (!user) {
+          user = await createUserFromGoogle(profile);
+        }
+
+        await deleteSessions(user.id);
+        const session = await createSession(user.id);
+        user.sessionToken = session.token;
+        return cb(null, user);
+      } catch (error) {
+        console.log("Error authenticating with google" + error);
+        return cb(error);
+      }
+    }
+  )
+);
+
+// Serialization: store onlt the session token
+passport.serializeUser((user, cb) => {
+  cb(null, user.sessionToken);
+});
+
+// Deserialization: get the user from the session token
+passport.deserializeUser(async (token, cb) => {
+  try {
+    const session = await findSessionByToken(toekn);
+    if (!session)
+      return cb(null, false, {
+        message: "Error findind the session for Deserialization",
+      });
+
+    const user = await findUserById(session.user_id);
+    if (!user)
+      return cb(null, false, {
+        message: "Error finding the user for Deserialization",
+      });
+    return cb(null, user);
+  } catch (error) {
+    console.log("Error Deserialization of the user" + error);
+    return cb(error);
+  }
+});
