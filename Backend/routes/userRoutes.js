2f41fec13a4d1da187bd46f8c2b24fd738e86479
Dhairya <dhairyadave962@gmail.com>
2025-06-02 20:09:26 +0530

    Create a post route for form handeling

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -17,16 +17,54 @@ userRouter.get("/profile", async (req, res) => {
   }
 
   try {
-    const userId = req.user.id
+    const userId = req.user.id;
 
-    const result = await dbPool.query("SELECT username, email, created_at FROM users WHERE id = $1", [userId]);
+    const result = await dbPool.query(
+      "SELECT username, email, created_at FROM users WHERE id = $1",
+      [userId]
+    );
 
     if (result.rows.length === 0) {
-      return res.send({ success: false, message: "User not found from userRouter" });
+      return res.send({
+        success: false,
+        message: "User not found from userRouter",
+      });
     }
     res.json({ success: true, user: result.rows[0] });
   } catch (error) {
     console.error("Error fetching user profile:", error);
+    res.send({ success: false, message: "Server error from userRouter" });
+  }
+});
+
+userRouter.put("/profile", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.send({
+      success: false,
+      message: "Unauthorized req from userRouter",
+    });
+  }
+
+  const { username, email } = req.body;
+
+  try {
+    const userId = req.user.id;
+
+    const result = await dbPool.query(
+      "UPDATE users SET username = $1, email = $2 WHERE id = $3 RETURNING *",
+      [username, email, userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.send({
+        success: false,
+        message: "User not found from userRouter",
+      });
+    }
+
+    res.json({ success: true, user: result.rows[0] });
+  } catch (error) {
+    console.error("Error updating user profile:", error);
     res.send({ success: false, message: "Server error from userRouter" });
   }
 });
