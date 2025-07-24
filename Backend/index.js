3c78ab1ce15994d4bdffba99f79b49e97ed07495
Dhairya <dhairyadave962@gmail.com>
2025-07-24 11:25:05 +0530

    Fix index file for production CORS

--- Backend/index.js
+++ Backend/index.js
@@ -8,21 +8,37 @@ import "dotenv/config";
 import cors from "cors";
 import session from "express-session"; // used to establish session using express
 import connectPgSimple from "connect-pg-simple"; // used to manage sessions in postgres database
-import dbPool from "./config/db.js";  
+import dbPool from "./config/db.js";
 import passport from "passport";
 import { authRouter } from "./routes/authRoutes.js";
 import { userRouter } from "./routes/userRoutes.js";
 import { bookRouter } from "./routes/booksRoutes.js";
-import { libraryRouter } from "./routes/libraryRoutes.js"
+import { libraryRouter } from "./routes/libraryRoutes.js";
 import "./config/passport.js";
 //import { insertBooks } from "./models/bookModel.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const PgSession = connectPgSimple(session); // decalration of the session management
+const PgSession = connectPgSimple(session); // decalration of the session management\
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://rate-n-read.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {  
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow credentials to be sent
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -39,7 +55,7 @@ app.use(
     cookie: {
       httpOnly: true,
       sameSite: "lax",
-      secure: false, // Set to true if using HTTPS
+      secure: true, // Set to true if using HTTPS
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
   })
@@ -53,8 +69,7 @@ app.use(passport.session());
 app.use("/api", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/books", bookRouter);
-app.use("/api/library", libraryRouter)
-
+app.use("/api/library", libraryRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
