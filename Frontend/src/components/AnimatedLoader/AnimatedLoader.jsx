ecd6bc653e0e89986df4d9f868d89b9d2305181b
Dhairya <dhairyadave962@gmail.com>
2025-07-31 09:44:33 +0530

    MAke the loader more UI firendly

--- Frontend/src/components/AnimatedLoader/AnimatedLoader.jsx
+++ Frontend/src/components/AnimatedLoader/AnimatedLoader.jsx
@@ -1,9 +1,22 @@
-const AnimatedLoader = () => {
+import { motion, AnimatePresence } from "framer-motion";
+
+const AnimatedLoader = ({ isLoading }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-blue-800 transition-opacity duration-700 ease-in-out bg-white">
-      <div className="w-12 h-12 mb-4 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
-      <p className="text-lg font-semibold animate-pulse">Loading your profile...</p>
-    </div>
+    <AnimatePresence>
+      {isLoading && (
+        <motion.div
+          key="loader"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="flex flex-col items-center justify-center min-h-screen text-blue-800 bg-white"
+        >
+          <div className="w-12 h-12 mb-4 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+          <p className="text-lg font-semibold animate-pulse">Loading your profile...</p>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
