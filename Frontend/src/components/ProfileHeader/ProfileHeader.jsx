9dabaa83bf2d6147217b1501a062ac59046b56dc
Dhairya <dhairyadave962@gmail.com>
2025-08-01 16:08:35 +0530

    Optimize coponent to reduce api call

--- Frontend/src/components/ProfileHeader/ProfileHeader.jsx
+++ Frontend/src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,39 +1,16 @@
-import React, { useState, useEffect, useContext } from "react";
-import { toast } from "react-toastify";
-import axios from "axios";
+import { useContext } from "react";
 import { AppContext } from "../../context//AppContext"
 
 function ProfileHeader() {
-
-  const [userName, setUsername] = useState("");
-  const { backendUrl } = useContext(AppContext);
-
-  useEffect(() => {
-    const fetchUserName = async () => {
-      try {
-        const response = await axios.get(`${backendUrl}/api/user/profile`, { withCredentials: true })
-
-        if (response.data.success === false) {
-          toast.error("Failed to fetch user profile. Please try again later.");
-          return;
-        }
-        const data = await response.data;
-        setUsername(data.user.username);
-      } catch (error) { 
-        console.error("Error fetching username:", error);
-        toast.error("Failed to fetch username. Please try again later.");
-      }
-    }
-
-    fetchUserName()
-  }, [])
+  
+  const { user } = useContext(AppContext);
   
   return (
     <div className="mb-8">
       <h1 className="mb-2 text-3xl font-bold">
         Hello,{" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-main-border to-ternary-pink">
-          {userName || "User"}!
+          {user?.username || "User"}!
         </span>
       </h1>
       <p className="text-text-mute">
